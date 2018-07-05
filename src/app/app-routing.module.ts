66477e73bf9c8d0125365583cccb49c015eb561f
Sutton Yamanashi <sutton.yamanashi@xpo.com>
2018-07-05 12:20:13 -0400

    Up to Listing 9.14 - Add an auth service, auth spec, and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'signup', loadChildren: 'src/app/signup/signup.module#SignupModule' },
 ];
 
 @NgModule({
